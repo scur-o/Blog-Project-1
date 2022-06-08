10357f42cfb40a4d1e108e92960bce36bbf664fb
Oscar <osscaaryyip@gmail.com>
2022-06-08 11:23:09 -0700

    change os.environ to get data from postgres db

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ gravatar = Gravatar(app,
 
 # ------------------ CONNECT DB ------------------ #
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SECRET_KEY'), 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI'), 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
